0467eb5d180d10dd8c158772414aba3e8aba74ea
Zakry Zapata <zakry.zapata@gmail.com>
2015-07-30 03:44:00 -0500

    Updated cipher

--- caesar_cipher.rb
+++ caesar_cipher.rb
@@ -1,12 +1,21 @@
 def caesar_cipher(message,key)
-		message = message.split(' ')
-
-		message.each do |word|
-		
-			word = word.split('')
-			word.map {|letter| letter = letter.next!}
-			word = word.join('')
-			puts word
-		end
-	
-end
+    message = message.split(' ')
+    message.map! do |word|      
+        word = word.split('')           
+        word.map! do |letter| # or word = word.map                
+            key.times do                    
+                case letter
+                when "z"
+                    letter = "a"
+                when "Z"
+                    letter = "A"
+                else 
+                    letter = letter.next!
+                end
+            end
+            letter # return the next letter from the block
+        end
+        word.join('')
+    end
+    message.join(' ')
+end 
